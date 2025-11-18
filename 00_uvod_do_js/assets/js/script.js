e342515c629a1cda00aa94122b94fdd65e0caee6
Jozef Dubec <127235676+srandykopec@users.noreply.github.com>
2025-11-18 09:26:21 +0100

    tlačidlo funguje

--- 00_uvod_do_js/assets/js/script.js
+++ 00_uvod_do_js/assets/js/script.js
@@ -1,13 +1,13 @@
 console.log('JS asi už funguje');
 
 
-const btn = document.getElementById('btn');
+const btn = document.getElementById("btn");
 
+const text = document.getElementById("text");
 
-
-if (btn){
+if (btn && text){
     btn.addEventListener("click",() => {
-        
-    })
+        text.textContent = "Nový obsah, koý som zmenil cez JavaScript!";
+    });
 }
 
