719a8d82b17df7b5bbb955edf47b79c742efd9c8
Jozef Dubec <127235676+srandykopec@users.noreply.github.com>
2026-02-10 08:20:23 +0100

    vytvoril som prmenné

---
+++ 05_aplikacia_pripomienky/assets/js/script.js
@@ -0,0 +1,7 @@
+const addButton = document.getElementById('addButton');
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+
+
+
+console.log(addButton);
