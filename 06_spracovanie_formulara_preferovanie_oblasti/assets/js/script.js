efe30c85212b9dd11c60d2ccab926b297d84aa7c
Jozef Dubec <127235676+srandykopec@users.noreply.github.com>
2026-03-17 09:32:42 +0100

    môžeme ísť na json

--- 06_spracovanie_formulara_preferovanie_oblasti/assets/js/script.js
+++ 06_spracovanie_formulara_preferovanie_oblasti/assets/js/script.js
@@ -17,10 +17,40 @@ function handleSubmit(event){
 
   const poleCheckboxov = Array.from(vsetkyCheckboxy); //prevod nodelisu (získali sme ho querySelectorAll) na pole (aby sme ho mohli filrovať)
 
+  // console.log("Polecheckboxov: " + poleCheckboxov)
+
   const zaskrtnute = poleCheckboxov.filter(checkbox => checkbox.checked) //vytiahn s poľa checkboxov iba tie, ktoré sú zaškrnuté 
 
-    const vybraneTexy = zaskrtnute.map(checkbox => checkbox.value);
-    console.log('Vybrané témy:', vybraneTexy);
-};
+  // console.log("zaskrtnute: " + zaskrtnute)
 
 
+  const vybraneTexy = zaskrtnute.map(checkbox => checkbox.value);
+
+  // console.log('Vybrané témy:', vybraneTexy);
+    
+  const ineOblasti =  document.getElementById("ineOblasti").value.trim();
+    
+
+  if (ineOblasti){ // ak nie je prázdne
+    vybraneTexy.push(ineOblasti);  //pridaj mi položku iné do poľa vybraneTexty
+  }
+    
+  const temyText = vybraneTexy.join(', ');
+  console.log(temyText);
+
+  function zobrazSpravu(text){
+    const spravaElementu = document.getElementById('sprava');
+
+    const odstavec = document.createElement('p');
+
+    odstavec.textContent = text;
+
+    spravaElementu.appendChild(odstavec);
+
+    spravaElementu.className = 'sprava popis';
+  }
+
+  zobrazSpravu(`Ďakujem ${meno}, tu sú tvoje preferované oblasti: ${temyText}`);
+
+
+  };
